70dd9b3ff38bf6b5b35e5a2f142d204cb8a9499d
Tyxik <tylmaxim@gmail.com>
2025-09-18 18:30:22 +0200

    first commit

---
+++ insert.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root"; // MAMP má výchozí heslo "root"
+$dbname = "mojedb";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Připojení selhalo: " . $conn->connect_error);
+}
+
+$jmeno = $_POST['jmeno'];
+$email = $_POST['email'];
+$barva = $_POST['barva'];
+
+$sql = "INSERT INTO uzivatele (jmeno, email, barva) VALUES ('$jmeno', '$email', '$barva')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "<p>✅ Data byla uložena. <a href='form.html'>Zpět na formulář</a></p>";
+} else {
+  echo "❌ Chyba: " . $conn->error;
+}
+
+$conn->close();
+?>
